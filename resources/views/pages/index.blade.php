a9e6b45266bf13cd0b52454d5299136093c445c7
SunnyChopper <ishy.singh@gmail.com>
2019-01-15 01:35:35 -0600

    Made new changes from client

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -66,8 +66,9 @@
 	<div class="container pt-64 pb-64" id="events">
 		<div class="row">
 			<div class="col-12">
-				<h2 class="text-center" style="color: #D7B056;">Build Your Coaching & Speaking Empire Tour 2019</h2>
-				<p class="text-center" style="line-height: 1.15em;"><small>Tickets are non-refundable. However, they are transferrable, which means that you can sell or gift your seat to someone else.</small>
+				<h2 class="text-center" style="color: #D7B056; margin-bottom: 16px;">Build Your Coaching & Speaking Empire Tour 2019</h2>
+				<p class="text-center" style="line-height: 1.15em;"><small>Tickets are non-refundable. However, they are transferrable, which means that you can sell or gift your seat to someone else.</small></p>
+				<p class="text-center" style="line-height: 1.15em;"><small>We will gladly change the name on the seat for you if we are notified at least 3 days before the event. If you would like to transfer your ticket, please email support@DrSonja.com at least 3 days prior to the event to update the registration.</small>
 				<hr />
 			</div>
 		</div>
@@ -80,7 +81,7 @@
 					<h4>California</h4>
 					<h5>January 26th, 2019</h5>
 					<p class="time">1PM to 5PM</p>
-					<p>Specific location will be sent through email after registration</p>
+					<p>Specific location will be provided after registration.</p>
 					<h4 class="price">$67<small>/<small>ticket</small></small></h4>
 					<a href="https://labuildyourempiretour.securechkout.com/" class="btn btn-primary mt-16">Purchase Ticket <i class="fas fa-arrow-circle-right ml-2"></i></a>
 				</div>
@@ -93,7 +94,7 @@
 					<h4>Texas</h4>
 					<h5>February 9th, 2019</h5>
 					<p class="time">1PM to 5PM</p>
-					<p>Specific location will be sent through email after registration</p>
+					<p>Specific location will be provided after registration.</p>
 					<h4 class="price">$47<small>/<small>ticket</small></small></h4>
 					<a href="https://houstonbuildyourempire.securechkout.com/" class="btn btn-primary mt-16">Purchase Ticket <i class="fas fa-arrow-circle-right ml-2"></i></a>
 				</div>
@@ -106,7 +107,7 @@
 					<h4>England</h4>
 					<h5>February 17th, 2019</h5>
 					<p class="time">1PM to 5PM</p>
-					<p>Specific location will be sent through email after registration</p>
+					<p>Specific location will be provided after registration.</p>
 					<h4 class="price">$67<small>/<small>ticket</small></small></h4>
 					<a href="https://londonbuildyourempire.securechkout.com/" class="btn btn-primary mt-16">Purchase Ticket <i class="fas fa-arrow-circle-right ml-2"></i></a>
 				</div>
@@ -121,7 +122,7 @@
 					<h4>Georgia</h4>
 					<h5>February 23rd, 2019</h5>
 					<p class="time">1PM to 5PM</p>
-					<p>Specific location will be sent through email after registration</p>
+					<p>Specific location will be provided after registration.</p>
 					<h4 class="price">$47<small>/<small>ticket</small></small></h4>
 					<a href="https://atlantabuildyourempire.securechkout.com/" class="btn btn-primary mt-16">Purchase Ticket <i class="fas fa-arrow-circle-right ml-2"></i></a>
 				</div>
@@ -134,7 +135,7 @@
 					<h4>District of Columbia</h4>
 					<h5>March 2nd, 2019</h5>
 					<p class="time">1PM to 5PM</p>
-					<p>Specific location will be sent through email after registration</p>
+					<p>Specific location will be provided after registration.</p>
 					<h4 class="price">$67<small>/<small>ticket</small></small></h4>
 					<a href="https://dcbuildyourempire.securechkout.com/" class="btn btn-primary mt-16">Purchase Ticket <i class="fas fa-arrow-circle-right ml-2"></i></a>
 				</div>
@@ -147,7 +148,7 @@
 					<h4>New Jersey</h4>
 					<h5>March 9th, 2019</h5>
 					<p class="time">1PM to 5PM</p>
-					<p>Specific location will be sent through email after registration</p>
+					<p>Specific location will be provided after registration.</p>
 					<h4 class="price">$67<small>/<small>ticket</small></small></h4>
 					<a href="https://njbuildyourempire.securechkout.com/" class="btn btn-primary mt-16">Purchase Ticket <i class="fas fa-arrow-circle-right ml-2"></i></a>
 				</div>
@@ -162,9 +163,48 @@
 					<h4>Florida</h4>
 					<h5>March 16th, 2019</h5>
 					<p class="time">1PM to 5PM</p>
-					<p>Specific location will be sent through email after registration</p>
+					<p>Specific location will be provided after registration.</p>
 					<h4 class="price">$47<small>/<small>ticket</small></small></h4>
 					<a href="https://fortlauderdalebuildyourempire.securechkout.com/" class="btn btn-primary mt-16">Purchase Ticket <i class="fas fa-arrow-circle-right ml-2"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div style="background: hsl(0, 0%, 95%);">
+		<div class="container pt-64 pb-64">
+			<div class="row" style="display: flex;">
+				<div class="col-lg-6 col-md-6 col-sm-8 col-12" style="margin: auto;">
+					<img src="{{ URL::asset('img/Who-is.jpg') }}" class="regular-image">
+				</div>
+
+				<div class="col-lg-6 col-md-6 col-sm-8 col-12" style="margin: auto;">
+					<h3>Who is Dr. Sonja?</h3>
+					<hr />
+					<p>Dr. Sonja Stribling is a Transformation Specialist, Inﬂuence Expert and Best-Selling Author known as the General to Giants. This retired Army Major, TV personality on Bravo Network, now uses this training and experience to lead thousands to their victory in dominating in their space and use their inner power to ascend to their Next Level in life and business.  As the Chairwoman of the Born to Be Powerful® Academy and Founder of Women Speak and Grow Rich® Masterclass, her firm’s courses, events, and results-based coaching programs, equips emerging and established influencers, entrepreneurs and game changers to use their life experience and knowledge to build their empires and leave a legacy. she shares her trade secrets to how she generated $250K in one day, grew her business to $1M in 12 months and generates over $100K monthly as a Coach and Speaker.</p>
+				</div>
+			</div>
+
+			<hr />
+
+			<div class="row justify-content-center mt-32">
+				<div class="col-lg-8 col-md-10 col-sm-12 col-12">
+					<p>As a mother of 3 boys and a divorcee after being married for 18 years, I know all to well the mindset of a woman who wanted more but just didn’t know what that more was or how to get to it. But one thing I knew after living the life I lived of:</p>
+					<ul>
+						<li>Having a child at 15 years old</li>
+						<li>Raped and left for dead at 17 years old</li>
+						<li>Joining the military and retiring after serving my Country for 21 years</li>
+						<li>Being a combat veteran (serving 3 combat tours one to include Iraq for 15 month)</li>
+						<li>Being married for 18yrs and going through a divorce that took 3 years to dissolve</li>
+						<li>Losing houses and cars</li>
+						<li>Being clinically depressed (partially losing my natural mind)</li>
+					</ul>
+					<p>Yes, I know all this sounds so traumatic but my point of sharing this information and my personal journal of my life is to show you that although we may experience tragedy in our lives, we can still muster up courage and strength and use the negative energy to create a life we deserve and desire. But it takes an inner power that many are challenged with discovering.</p>
+					<p>You see less than 3 years age I decided to not focus on me and begin helping women through their life journey. This quickly turned into a million-dollar business. Yes million- dollar BUSINESS!! I had to win because for so long I wasn’t winning. I had to give myself permission to be AMAZING!</p>
+					<p>Today I help thousands of women to have fulfilled lives by digging deep and discovery why they aren’t WINNING in life or business.</p>
+					<p>The one thing I have found is many aren’t even on a pathway to their purpose.</p>
+					<p>I want to see you WIN in your Destiny and operating in your PURPOSE. That is why I get up every day to find women like you.</p>
+					<p>I wanted to become the woman to many, that I didn’t have in my life during my life journey to greatness.</p>
 				</div>
 			</div>
 		</div>
